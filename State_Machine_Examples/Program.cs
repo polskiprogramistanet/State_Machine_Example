d0dd2ac183667f99a57b773aefa37837cd1ebdc6
polskiprogramistanet <rav@polskiprogramista.net>
2021-09-17 14:50:47 +0200

    zakończenie

--- State_Machine_Examples/Program.cs
+++ State_Machine_Examples/Program.cs
@@ -10,16 +10,27 @@ namespace State_Machine_Examples
     {
         static void Main(string[] args)
         {
-            var phoneCall = new PhoneCall("Stefan");//połaczenie telefoniczne
+            var phoneCall = new PhoneCall("Rafał");//połaczenie telefoniczne
             phoneCall.Print();
             phoneCall.Dialed("Karol");
             phoneCall.Print();
             phoneCall.Connected();
             phoneCall.Print();
-
+            phoneCall.SetVolume(2);
+            phoneCall.Print();
+            phoneCall.Hold();
+            phoneCall.Print();
+            phoneCall.Mute();
+            phoneCall.Print();
+            phoneCall.UnMute();
+            phoneCall.Print();
+            phoneCall.Resume();
+            phoneCall.Print();
+            phoneCall.SetVolume(11);
+            phoneCall.Print();
             //Console.WriteLine(phoneCall.ToDotGraph());
 
-            Console.WriteLine("Press any key...");
+            //Console.WriteLine("Press any key...");
             Console.ReadKey(true);
         }
     }
